7f12bfdf088bd843938a0f5d805c7dea010305cd
fGHG99 <azkalearn10@gmail.com>
2023-12-05 21:41:37 +0700

    Fetch data dari database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,41 @@
-import classes from "./AvailableMeals.module.css";
-import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "1",
-    name: "Chicken Steak",
-    description: "Chicken Steak dengan brown sauce",
-    price: 45.000,
-  },
-  {
-    id: "2",
-    name: "Spaghetti",
-    description: "Spaghetti dengan saus Bolognese",
-    price: 35.000,
-  },
-  {
-    id: "3",
-    name: "Wagyu Steak",
-    description: "Daging Wagyu Import",
-    price: 60.000,
-  },
-  {
-    id: "4",
-    name: "Es Teh Manis Dingin/Panas",
-    description: "Teh manis ada dingin dan panas",
-    price: 10.000,
-  },
-];
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [menus, setMenus] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await axios.get('http://localhost:3333/menus'); // Replace with your actual API endpoint
+        setMenus(response.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError('Failed to fetch meals.');
+        setIsLoading(false);
+      }
+    };
+
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  const mealsList = menus.map((meal) => (
     <MealItem
-      id={meal.id}
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
